b5daf1b84297b2090079394e34d50090846835c3
ishandahal <ishan.dahal88@gmail.com>
2024-05-21 18:31:36 -0400

    Add StackPush and StackLen

--- src/stack/stack.c
+++ src/stack/stack.c
@@ -1,16 +1,51 @@
 #include "stack.h"
+#include <assert.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 #define START_SIZE 4
+#define DOUBLE 2
 
 /* Initialize stack with allocation from the heap for START_SIZE integers. */
 void StackNew(Stack *container) {
-	container->elems = malloc(START_SIZE * sizeof(int));
-	container->logicalLen = 0;
-	container->allocLen = START_SIZE;
+  container->elems = malloc(START_SIZE * sizeof(int));
+  assert(container->elems != NULL); // Check if memory allocation succeded
+  container->logicalLen = 0;
+  container->allocLen = START_SIZE;
+}
+
+/* Free allocated memory for the stack and reset logical and allocated lengths
+ * to 0 */
+void StackDispose(Stack *container) {
+  free(container->elems);
+  container->allocLen = 0;
+  container->logicalLen = 0;
+}
+
+void StackPush(Stack *container, int num) {
+  // Need to allocate more memory. Let's use doubling strategy
+  if (container->logicalLen >= container->allocLen) {
+    container->elems = realloc(container->elems, container->allocLen * DOUBLE);
+    container->allocLen *= DOUBLE;
+  }
+  *(container->elems + container->logicalLen) = num;
+  container->logicalLen++;
+}
+
+int StackLen(Stack *container) {
+	return container->logicalLen;
 }
 
 int main() {
-	Stack stack;
-
+  Stack stack;
+  StackNew(&stack);
+  printf("Allocated memory to stack\n");
+	// Add 5 items to stack
+	for (int i = 0; i < 10; i++) {
+		StackPush(&stack, i);
+	}
+	printf("Length of stack after fill: %d\n", StackLen(&stack));
+  StackDispose(&stack);
+  printf("Disposed stack\n");
+	printf("Length of stack after disposal: %d\n", StackLen(&stack));
 }
--- src/stack/stack.h
+++ src/stack/stack.h
@@ -1,11 +1,12 @@
 
 typedef struct {
-	int *elems;
-	int logicalLen;
-	int allocLen;
+  int *elems;
+  int logicalLen;
+  int allocLen;
 } Stack;
 
 void StackNew(Stack *);
 void StackDispose(Stack *);
 void StackPush(Stack *, int);
 int StackPop(Stack *);
+int StackLen(Stack *);
