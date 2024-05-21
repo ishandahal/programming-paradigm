d281aa19516a6cdeca34d6f19b725ab16b81bfb9
ishandahal <ishan.dahal88@gmail.com>
2024-05-21 18:50:25 -0400

    Add StackPop function
    Does not downscale allocated memory.

--- src/stack/stack.c
+++ src/stack/stack.c
@@ -22,6 +22,8 @@ void StackDispose(Stack *container) {
   container->logicalLen = 0;
 }
 
+/* Push ints on the stack. Increase memory by doubling strategy if no space is
+ * available.*/
 void StackPush(Stack *container, int num) {
   // Need to allocate more memory. Let's use doubling strategy
   if (container->logicalLen >= container->allocLen) {
@@ -32,20 +34,32 @@ void StackPush(Stack *container, int num) {
   container->logicalLen++;
 }
 
-int StackLen(Stack *container) {
-	return container->logicalLen;
+/* Return number of elements on the stack */
+int StackLen(Stack *container) { return container->logicalLen; }
+
+int StackPop(Stack *container) {
+  // Array indexing start at 0 but logicalLen is 1 ahead
+  int res = *(container->elems + container->logicalLen - 1);
+  container->logicalLen--;
+  return res;
 }
 
 int main() {
   Stack stack;
   StackNew(&stack);
   printf("Allocated memory to stack\n");
-	// Add 5 items to stack
-	for (int i = 0; i < 10; i++) {
-		StackPush(&stack, i);
-	}
-	printf("Length of stack after fill: %d\n", StackLen(&stack));
+  // Add 5 items to stack
+  for (int i = 0; i < 10; i++) {
+    StackPush(&stack, i + 5);
+  }
+  printf("Length of stack after fill: %d\n", StackLen(&stack));
+
+  printf("Popping off: \n");
+  for (int i = 0; i < 5; i++) {
+    printf("Popped: %d\n", StackPop(&stack));
+    printf("Length of stack: %d\n", StackLen(&stack));
+  }
   StackDispose(&stack);
   printf("Disposed stack\n");
-	printf("Length of stack after disposal: %d\n", StackLen(&stack));
+  printf("Length of stack after disposal: %d\n", StackLen(&stack));
 }
