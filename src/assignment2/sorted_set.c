1eb8b3d44ee8321d04de20f2252ba05330e15f28
ishandahal <ishan.dahal88@gmail.com>
2024-05-25 06:48:52 -0400

    Complete SetNew

--- src/assignment2/sorted_set.c
+++ src/assignment2/sorted_set.c
@@ -1,10 +1,16 @@
-typedef struct{
-	int *root;
-	int logicalLen;
-	int allocLen;
-	int (*cmpfn) (void *, void *);
-	int elemSize;
+#include <assert.h>
+#include <stdlib.h>
+#include <time.h>
+
+typedef struct {
+  int *root;
+  int logicalLen;
+  int allocLen;
+  int (*cmpfn)(const void *, const void *);
+  int elemSize;
 } sortedset;
+
+#define NodeSize(elemSize) (2 * sizeof(int) + (elemSize))
 
 /*
  * Function: SetNew
@@ -18,4 +24,24 @@ typedef struct{
  */
 static const int kInitialCapacity = 4;
 void SetNew(sortedset *set, int elemSize,
- int (*cmpfn)(const void *, const void *));
+            int (*cmpfn)(const void *, const void *)) {
+  assert(cmpfn != NULL);
+  set->cmpfn = cmpfn; // Link the comparison function
+  assert(elemSize > 0);
+  // Allocate enough memory for odd intial int plus default capacity
+  set->root = malloc(sizeof(int) + kInitialCapacity * NodeSize(elemSize));
+  assert(set->root != NULL);
+	*(set->root) = -1; // set is empty when initialized
+	set->logicalLen = 0;
+	set->allocLen = kInitialCapacity;
+	set->elemSize = elemSize;
+}
+
+int dummy_cmpfunc(const void* left, const void* right) {
+	return -1;
+}
+
+int main() {
+	sortedset s;
+	SetNew(&s, sizeof(s), dummy_cmpfunc);
+}
